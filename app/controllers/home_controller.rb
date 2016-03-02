fc30df04fa94c23d98cc6c36d76dbc6a3d566cf8
John Baycroft <jbaycroft@capaccio.com>
2016-03-02 15:39:36 -0800

    set up summer system

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,19 @@ class HomeController < ApplicationController
   # Home page for application
   def index
   end
+  # This will be to gather the two values that will be added together
+  # GET /summer
+  def summer
+  end
+  # This will return the two values that you wanted added as well as the sum of the two
+  # GET params[:val1] AND params[:val2] and return /sum
+  def sum
+    if params[:val1].blank? || params[:val2].blank?
+      @val1 = 0
+      @val2 = 0
+    else
+      @val1 = params[:val1].to_f
+      @val2 = params[:val2].to_f
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   root 'home#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+     get '/summer' => 'home#summer'
+     get '/sum' => 'home#sum'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
