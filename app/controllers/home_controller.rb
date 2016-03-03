c2038f972fdcfd48dbd4645d63dcc71b70f750ba
James Gaskell <jgaskell@ificlaims.com>
2016-03-03 14:08:18 -0500

    ....cleaned up

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -32,16 +32,22 @@ class HomeController < ApplicationController
   end
 
   def complex1
-  end
-
-  def complex2
-  if
+    if
+      params[:val1].blank? || params[:val2].blank? || params[:val3].blank? || params[:val4].blank? || params[:val5].blank? || params[:val6].blank?
+      @val1 = 0
+      @val2 = 0
+      @val3 = 0
+      @val4 = 0
+      @val5 = 0
+      @val6 = 0
+    else
       @val1 = params[:val1].to_f
       @val2 = params[:val2].to_f
       @val3 = params[:val3].to_f
       @val4 = params[:val4].to_f
       @val5 = params[:val5].to_f
       @val6 = params[:val6].to_f
+    end
+   end
+
   end
-end
-end
